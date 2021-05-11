26a0bd6df7c3f7576940142f6e4080d93c537fbf
Thijs Nijhof <tnijhof91@gmail.com>
2021-05-11 12:03:21 +0100

    base style for searchform

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,7 +10,7 @@ const Header : React.FunctionComponent<Props> = (props:Props) => {
     return(
         <header className="lg:min-h-160 md:min-h-160 sm:min-h-136 bg-no-repeat relative" style={{ backgroundImage: `url(${bg}), linear-gradient(to right, transparent 0%, #5964E0 50%)`, objectFit: 'fill' }}>
                 <h1 className="text-white text-xxl font-sans pt-5 ml-16">{props.title}</h1>
-            <SearchForm classes="absolute inset-x-1/4 -bottom-4"/>
+            <SearchForm classes="absolute -bottom-4 "/>
         </header>
     )
 }
